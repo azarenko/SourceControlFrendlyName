f9cf547af04103896523b573ee522136354022ff
Sergey Azarenko <azarenko.s@gmail.com>
2022-11-24 16:29:49 +0200

    init commit

---
+++ SourceControlFrendlyName/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace SourceControlFrendlyName
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this._input = new System.Windows.Forms.TextBox();
+            this._output = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this._input, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this._output, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(796, 198);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // _input
+            // 
+            this._input.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._input.Location = new System.Drawing.Point(3, 3);
+            this._input.Multiline = true;
+            this._input.Name = "_input";
+            this._input.Size = new System.Drawing.Size(790, 93);
+            this._input.TabIndex = 0;
+            this._input.TextChanged += new System.EventHandler(this._input_TextChanged);
+            // 
+            // _output
+            // 
+            this._output.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._output.Location = new System.Drawing.Point(3, 102);
+            this._output.Multiline = true;
+            this._output.Name = "_output";
+            this._output.ReadOnly = true;
+            this._output.Size = new System.Drawing.Size(790, 93);
+            this._output.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 198);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Frendly Name";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox _input;
+        private System.Windows.Forms.TextBox _output;
+    }
+}
+
---
+++ SourceControlFrendlyName/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SourceControlFrendlyName
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void _input_TextChanged(object sender, EventArgs e)
+        {
+            _output.Text = PrepareString(_input.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText(TextDataFormat.Text))
+            {
+                _input.Text = Clipboard.GetText(TextDataFormat.Text);
+            }
+        }
+
+        private string PrepareString(string input)
+        {
+            Regex re = new Regex("[^a-zA-Z0-9\\n]", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            return re.Replace(input, "_");
+        }
+    }
+}
